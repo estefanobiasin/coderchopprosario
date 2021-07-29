d8a844861a8a92105a14e43eb7021691453f544d
estefanobiasin <64477977+estefanobiasin@users.noreply.github.com>
2021-07-29 20:13:47 -0300

    entrega 3

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ItemListContainer extends React.Component{
+
+    render (){
+        return(
+           <h1>Lista de elementos</h1>
+        )
+    }
+
+}
+export default ItemListContainer
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,16 +3,11 @@ import './NavBar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar,Nav, NavDropdown, Container} from 'react-bootstrap';
 import logo from './logo.png';
+import CartWidget from "./CartWidget";
 
 function NavBar(){
-    // return <div className="title">Chopp Rosario  
-    //         <a href="#" >Choperas</a>
-    //         <a href="#">Barriles</a>
-    //         <a href="#"> Servicios </a>
-    //         <a href="#"> Tienda </a>
-    //         <button className="btn btn-primary">Boton</button>
-    // </div>
-    return <div>
+    
+return <div>
     <Navbar collapseOnSelect expand="lg" className='title'>
   <Container>
    <img src={logo} className="img-navbar"></img>
@@ -20,9 +15,10 @@ function NavBar(){
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
       <Nav.Link href="#features" >Inicio</Nav.Link>
-      <Nav.Link href="#pricing">Choperas</Nav.Link>
+      <Nav.Link href="#features" >Choperas</Nav.Link>
       <Nav.Link href="#features" >Barriles</Nav.Link>
       <Nav.Link href="#features" >TIENDA</Nav.Link>
+      
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -30,12 +26,15 @@ function NavBar(){
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
+      
     </Nav>
     <Nav>
       <Nav.Link href="#deets">Contacto</Nav.Link>
       <Nav.Link eventKey={2} href="#memes">
         Ubicacion
+     
       </Nav.Link>
+      <Nav.Link href="#" ><CartWidget/></Nav.Link> 
     </Nav> 
   </Navbar.Collapse> 
   </Container>
